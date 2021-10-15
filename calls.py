8b93cf4936c780eaddce00a76d6b4749cff40ec4
rob <hassenmayerr@gmail.com>
2021-10-14 21:20:37 -0400

    debugged refactor

--- calls.py
+++ calls.py
@@ -4,11 +4,11 @@ import os
 import sys
 import glob
 
-def new_file():
+def new_file(pad_path):
     print("Chosen option: Create a new file \n")
     new_file_name = input("[+] New file name: ")
     new_file_name = new_file_name + ".txt"
-    current_file_path = notepad_file_path + "/" + new_file_name
+    current_file_path = pad_path + "/" + new_file_name
     print("Creating file: " + new_file_name)
     sub.call(["touch", current_file_path])
     sub.call("clear")
@@ -16,44 +16,44 @@ def new_file():
     os.system(f"cat > {current_file_path}")
     
     
-def add_to_file():
+def add_to_file(pad_path):
     print("Chosen option: Add to a file \n\n-* Files:")
-    os.system(f"ls -r --format=single-column {notepad_file_path}")
+    os.system(f"ls -r --format=single-column {pad_path}")
     chosen_file = input("[+] Select file: ")
     sub.call("clear")
     print("Adding to file: " + chosen_file + 
     "\n--------------------------------------------------------------------------------------------")
-    current_file_path = notepad_file_path + "/" + chosen_file
+    current_file_path = pad_path + "/" + chosen_file
     os.system(f"cat >> {current_file_path}")
     
-def overwrite_file():
+def overwrite_file(pad_path):
     print("Chosen option: Overwrite a file\n\n-* Files:")
-    os.system(f"ls -r --format=single-column {notepad_file_path}")
+    os.system(f"ls -r --format=single-column {pad_path}")
     chosen_file = input("[+] Select file: ")
     sub.call("clear")
     print("Overwriting file: " + chosen_file + 
     "\n--------------------------------------------------------------------------------------------")
-    current_file_path = notepad_file_path + "/" + chosen_file
+    current_file_path = pad_path + "/" + chosen_file
     os.system(f"cat > {current_file_path}")
 
 
-def read_file():
+def read_file(pad_path):
     print("Chosen option: Read a file\n\n-* Files:")
-    os.system(f"ls -r --format=single-column {notepad_file_path}")
+    os.system(f"ls -r --format=single-column {pad_path}")
     chosen_file = input("[+] Select file: ")
     sub.call("clear")
     print("Reading file: " + chosen_file + 
     "\n--------------------------------------------------------------------------------------------")
-    current_file_path = notepad_file_path + "/" + chosen_file
+    current_file_path = pad_path + "/" + chosen_file
     os.system(f"cat {current_file_path}")
     
     
-def delete_file():
+def delete_file(pad_path):
     print("Chosen option: Delete a file\n\n-* Files:")
-    os.system(f"ls -r --format=single-column {notepad_file_path}")
+    os.system(f"ls -r --format=single-column {pad_path}")
     chosen_file = input("[+] Select file: ")
     sub.call("clear")
     print("Deleting file: " + chosen_file)
-    current_file_path = notepad_file_path + "/" + chosen_file
+    current_file_path = pad_path + "/" + chosen_file
     os.system(f"rm {current_file_path}")
     print(f"Chosen file deleted")
--- notepad.py
+++ notepad.py
@@ -3,7 +3,7 @@ import subprocess as sub
 import os
 import sys
 import glob
-import calls.py
+from calls import *
 username = os.getlogin()
 notepad_file_path = "/home/" + username + "/Documents/notepad_files"
 
@@ -21,19 +21,19 @@ user_file_call = input(
 "Would you like to:\nCreate a new file? Type \"n\"\nAdd to a file? Type \"a\"\nOverwrite a file? Type \"o\"\nRead a file? Type \"r\"\nDelete a file? Type \"d\"\n[+] Options (n/a/o/r/d): ")
 sub.call("clear")
 if  user_file_call.upper() == "N":
-    new_file()
+    new_file(notepad_file_path)
 	
 elif user_file_call.upper() == "A":
-    add_to_file()
+    add_to_file(notepad_file_path)
 	
 elif user_file_call.upper() == "O":
-    overwrite_file()
+    overwrite_file(notepad_file_path)
     
 elif user_file_call.upper() == "R":
-    read_file()
+    read_file(notepad_file_path)
     
 elif user_file_call.upper() == "D":
-    delete_file()
+    delete_file(notepad_file_path)
 else:
     print("[-] Invalid Input: Please only type given options")
 	
